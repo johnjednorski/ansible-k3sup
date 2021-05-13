b24a3283333bf79799eb76f1f66668b79d2b81d2
JP Jednorski <john.jednorski@chainalysis.com>
2021-05-12 20:26:37 -0400

    minor updates like ipv4 ansible fact location and optional rpi config

--- play.sh
+++ play.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 HERE=$(pwd)
-ansible-playbook -i "$HERE"/inventory/hosts.ini --vault-password-file="$HERE"/.vaultpw -K main.yml
+ansible-playbook -i "$HERE"/inventory/hosts.ini --vault-password-file="$HERE"/.vaultpw -K main.yml $1
